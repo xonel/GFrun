45090e12258cfd7f065730a589b24aad3b89223f
Le.NoX <le.nox @ nox.fr>
2013-04-25 13:03:33 +0200

    Signed-off-by: Le.NoX <le.nox @ nox.fr>

--- resources/FIT-to-TCX-master/__init__.py
+++
@@ -1,4 +0,0 @@
-from fitparse.activity import Activity
-from fitparse.exceptions import FitError, FitParseError, FitParseComplete
-
-__all__ = ['Activity', 'FitError', 'FitParseError', 'FitParseComplete']
