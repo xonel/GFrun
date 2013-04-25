0004f2f080e546c00a2e05f71ac87071fd9e0d74
Le.NoX <le.nox @ nox.fr>
2013-04-25 13:07:27 +0200

    Signed-off-by: Le.NoX <le.nox @ nox.fr>

---
+++ resources/2013_04_25/fitparse/__init__.py
@@ -0,0 +1,4 @@
+from fitparse.activity import Activity
+from fitparse.exceptions import FitError, FitParseError, FitParseComplete
+
+__all__ = ['Activity', 'FitError', 'FitParseError', 'FitParseComplete']
