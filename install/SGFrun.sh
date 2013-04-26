46058d1dde819380b3a9dd298a5a73df5009d965
Le.NoX <le.nox @ nox.fr>
2013-04-26 21:10:48 +0200

    Signed-off-by: Le.NoX <le.nox @ nox.fr>

--- install/SGFrun.sh
+++ install/SGFrun.sh
@@ -67,9 +67,9 @@ NUMERO_DE_MA_MONTRE=$(ls $HOME/.config/garmin-extractor/ | grep -v Garmin | grep
 cd $HOME/.config/garmin-extractor/$NUMERO_DE_MA_MONTRE/ && ln -s $HOME/.config/garmin-extractor/$NUMERO_DE_MA_MONTRE/activities Activites && mv Activities $HOME/.config/garmin-extractor/Garmin/ 
 
 ##Configuration des fichiers de config avec le #HOME et le $NUMERO_DE_MA_MONTRE
-src=/path/to/FIT-to-TCX/fittotcx.py && cibl=$HOME/Garmin-Forerunner-610-Extractor-master/resources/FIT-to-TCX-master/fittotcx.py && echo -e "sed -i 's|$src|$cibl|g' $HOME/.config/garmin-extractor/scripts/40-convert_to_tcx.py" >>/tmp/ligneCmd.sh
-src=ID_MA_MONTRE && cibl=$NUMERO_DE_MA_MONTRE && echo -e "sed -i 's|$src|$cibl|g' $HOME/.config/garmin-extractor/Garmin/GarminDevice.xml" >>/tmp/ligneCmd.sh
-src=MON_HOME && cibl=$HOME && echo -e "sed -i 's|$src|$cibl|g' $HOME/.config/garminplugin/garminplugin.xml" >>/tmp/ligneCmd.sh
+src=/path/to/FIT-to-TCX/fittotcx.py && cibl=$HOME/Garmin-Forerunner-610-Extractor-master/resources/FIT-to-TCX-master/fittotcx.py && echo "sed -i 's|$src|$cibl|g' $HOME/.config/garmin-extractor/scripts/40-convert_to_tcx.py" >>/tmp/ligneCmd.sh
+src=ID_MA_MONTRE && cibl=$NUMERO_DE_MA_MONTRE && echo "sed -i 's|$src|$cibl|g' $HOME/.config/garmin-extractor/Garmin/GarminDevice.xml" >>/tmp/ligneCmd.sh
+src=MON_HOME && cibl=$HOME && echo "sed -i 's|$src|$cibl|g' $HOME/.config/garminplugin/garminplugin.xml" >>/tmp/ligneCmd.sh
 cd /tmp/ && chmod u+x /tmp/ligneCmd.sh && sh /tmp/ligneCmd.sh
 chown -R $SUDO_USER:$SUDO_USER $HOME/.config/garminplugin $HOME/.config/garmin-extractor $HOME/Garmin-Forerunner-610-Extractor-master
 chmod -R a+x $HOME/.config/garmin-extractor/scripts/ $HOME/Garmin-Forerunner-610-Extractor-master/scripts/
