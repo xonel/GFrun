f85423ba084ecdea1554573077a20388273ba82c
Le.NoX <le.nox @ nox.fr>
2013-04-27 01:32:20 +0200

    Signed-off-by: Le.NoX <le.nox @ nox.fr>

--- install/SGFrun.sh
+++ install/SGFrun.sh
@@ -63,13 +63,16 @@ cd $HOME/.config/ && unzip -o _.GFrunGarminplugin.zip
 ##1ere connection GExtractor recup $NUMERO_DE_MA_MONTRE
 xterm -e 'cd $HOME/Garmin-Forerunner-610-Extractor-master/ && python ./garmin.py'
 NUMERO_DE_MA_MONTRE=$(ls $HOME/.config/garmin-extractor/ | grep -v Garmin | grep -v scripts)
-cd $HOME/.config/garmin-extractor/$NUMERO_DE_MA_MONTRE/ && ln -s $HOME/.config/garmin-extractor/$NUMERO_DE_MA_MONTRE/activities Activites && mv Activities $HOME/.config/garmin-extractor/Garmin/ 
+cd $HOME/.config/garmin-extractor/$NUMERO_DE_MA_MONTRE/ && ln -s $HOME/.config/garmin-extractor/$NUMERO_DE_MA_MONTRE/activities Activites && mv Activities $HOME/.config/garmin-extractor/Garmin/Activities
 
 ##Configuration des fichiers de config avec le #HOME et le $NUMERO_DE_MA_MONTRE
 src=/path/to/FIT-to-TCX/fittotcx.py && cibl=$HOME/Garmin-Forerunner-610-Extractor-master/resources/FIT-to-TCX-master/fittotcx.py && echo "sed -i 's|$src|$cibl|g' $HOME/.config/garmin-extractor/scripts/40-convert_to_tcx.py" >> /tmp/ligneCmd.sh
+
 if [$NUMERO_DE_MA_MONTRE -n]; then
- src=ID_MA_MONTRE && cibl=$NUMERO_DE_MA_MONTRE && echo "sed -i 's|$src|$cibl|g' $HOME/.config/garmin-extractor/Garmin/GarminDevice.xml" >> /tmp/ligneCmd.sh
+	src=ID_MA_MONTRE && cibl=$NUMERO_DE_MA_MONTRE
+	echo "sed -i 's|$src|$cibl|g' $HOME/.config/garmin-extractor/Garmin/GarminDevice.xml" >> /tmp/ligneCmd.sh
 fi
+
 src=MON_HOME && cibl=$HOME && echo "sed -i 's|$src|$cibl|g' $HOME/.config/garminplugin/garminplugin.xml" >> /tmp/ligneCmd.sh
 cd /tmp/ && chmod u+x /tmp/ligneCmd.sh && sh /tmp/ligneCmd.sh
 chown -R $SUDO_USER:$SUDO_USER $HOME/.config/garminplugin $HOME/.config/garmin-extractor $HOME/Garmin-Forerunner-610-Extractor-master
