02912758c1aa44305214cb374c634401a1cb6965
Le.NoX <le.nox @ nox.fr>
2013-04-27 04:36:35 +0200

    Signed-off-by: Le.NoX <le.nox @ nox.fr>

---
+++ Make_GFrunOffline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Configuration file for GARMIN FORERUNNER - GFrun
+# Modif by Le.NoX
+#
+############################
+#     Auteurs : Le.NoX ;o) 
+#      Version="0.1"
+#     Licence: GNU
+############################
+# zip fichier de config offline (GFrunOffline.zip) + config garminplugin (_.GFrunGarminplugin.zip)
+zip -r GFrunOffline.zip  master.zip _.config/ Garmin-Forerunner-610-Extractor-master/
+cd _.config/ && zip -r _.GFrunGarminplugin.zip garmin-extractor/ garminplugin/
+echo "FIN"
