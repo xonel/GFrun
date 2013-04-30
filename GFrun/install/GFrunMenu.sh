8525c935aade215b6f116a6eb0e5eaa84bdfa3f6
Le.NoX <le.nox free.fr>
2013-04-30 21:28:14 +0200

    Signed-off-by: Le.NoX <le.nox free.fr>

--- GFrun/install/GFrunMenu.sh
+++ GFrun/install/GFrunMenu.sh
@@ -76,6 +76,8 @@ rm -f GFrun.sh* guploader.sh*
         echo "-----------"
         echo `color 35 "6. Extract.Fit >> PC..............(GFrun.sh -e)"`
         echo `color 35 "7. Upload.Fit >> garmin.com ......(GFrun.sh -u)"`
+        echo ""
+		echo `color 31 "EXIT : Select [X] and [enter]"`
 #        echo `color 32 "7. "`
 #        echo `color 32 "8. "`
 #        echo `color 32 "9. "`
