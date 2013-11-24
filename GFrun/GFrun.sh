c74663bf71690f0be7577aed1bce213151223f10
xonel <xonel@lenox.fr>
2013-11-24 19:43:05 +0100

    Signed-off-by: xonel <xonel@lenox.fr>

--- GFrun/GFrun.sh
+++ GFrun/GFrun.sh
@@ -292,8 +292,8 @@ F_clean_up(){
 	echo `color 32 ">>> F_clean_up"`
 	rm -f $HOME/Verror* $HOME/GFrun.sh* $HOME/master.zip* $HGFrun/tools/FIT-to-TCX/master.zip* $HGFrun/tools/master.zip* $HGFrun/tools/pygupload_20120516.zip* /tmp/ligneCmd.sh* 1>/dev/null
 	rm -fr $HOME/pyusb/ 1>/dev/null
-	cp -fr $HOME/GFrun_Backup/* 1>/dev/null $HGFrun/GFrun_Backup/ 1>/dev/null && rm -fr $HOME/GFrun_Backup 1>/dev/null
-	cp -f $HOME/logs/* 1>/dev/null $HGFrun/logs/ 1>/dev/null && rm -fr $HOME/logs 1>/dev/null
+	cp -fr $HOME/GFrun_Backup $HGFrun/GFrun_Backup 1>/dev/null && rm -fr $HOME/GFrun_Backup 1>/dev/null
+	cp -f $HOME/logs $HGFrun/logs 1>/dev/null && rm -fr $HOME/logs 1>/dev/null
 }
 
 F_Apt(){
