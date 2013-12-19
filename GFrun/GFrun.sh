b1ef9122ceae0010d10d8f6d0c909246a711ca44
xonel <xonel@lenovo.nox>
2013-12-20 00:59:38 +0100

    Signed-off-by: xonel <xonel@lenovo.nox>

--- GFrun/GFrun.sh
+++ GFrun/GFrun.sh
@@ -396,7 +396,8 @@ F_Git(){
 		fi
 		echo "$Vbranche : "$Vbranche
 		#cd $HOME && git clone -b $Vbranche https://github.com/xonel/GFrun.git 1>/dev/null
-		cd $HOME && wget -N https://codeload.github.com/xonel/GFrun/zip/$Vbranche 1>/dev/null && unzip -o $Vbranche 1>/dev/null && mv $HGFrun-$Vbranche $HGFrun 1>/dev/null
+		cd $HOME && wget -N https://codeload.github.com/xonel/GFrun/zip/$Vbranche 1>/dev/null && unzip -o $Vbranche 1>/dev/null
+		mkdir $HGFrun && cp $HGFrun-$Vbranche $HGFrun 1>/dev/null
 
 		cp -rf $HGFrun/_.config/* $HOME/.config/ && rm -r $HGFrun/_.config
 		cp -rf $HGFrun/_.local/* $HOME/.local/ && rm -r $HGFrun/_.local
